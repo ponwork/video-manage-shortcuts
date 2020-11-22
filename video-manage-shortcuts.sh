1b74d6711f2b6412571b909afef55862f80be3d6
ponwork <ponomarev.yuri@gmail.com>
2020-11-22 15:32:22 +0300

    comments updated

--- video-manage-shortcuts.sh
+++ video-manage-shortcuts.sh
@@ -1,3 +1,4 @@
+#====================================================
 # Create File List
 echo file file1.mp4 >  mylist.txt 
 echo file file2.mp4 >> mylist.txt
@@ -10,7 +11,8 @@ ffmpeg -f concat -i mylist.txt -c:a copy output.mp4
 ffmpeg -i input.mp4 -vf "ass=sub.ass" result.mp4
 
 #note: convert srt to ass first via: ffmpeg -i subtitles.srt subtitles.ass
-====================================================
+
+#====================================================
 #Crop 20 pixels from the top, and 20 from the bottom:
 
 ffmpeg -i in.mp4 -filter:v "crop=in_w:in_h-40" -c:a copy out.mp4
@@ -19,14 +21,12 @@ ffmpeg -i in.mp4 -filter:v "crop=in_w:in_h-40" -c:a copy out.mp4
 
 ffmpeg -i in.mp4 -filter:v "crop=in_w:in_h-40:0:out_h" -c:a copy out.mp4
 
-====================================================
-
+#====================================================
 # Cut 8 seconds of the video starting from 3rd second and save it as a file 
 
 ffmpeg -i movie.mp4 -ss 00:00:03 -t 00:00:08 -async 1 cut.mp4
 
-====================================================
-
+#====================================================
 # Extract Audio
 ffmpeg -i video.mp4 -f mp3 -vn music.mp3
 
@@ -39,7 +39,7 @@ ffmpeg -i INPUT.mp4 -codec copy -an OUTPUT.mp4
 # combine the two streams together (new audio with originally exisiting video)
 ffmpeg -i INPUT.mp4 -i AUDIO.wav -shortest -c:v copy -c:a aac -b:a 256k OUTPUT.mp4
 
-====================================================
+#====================================================
 # Reverse video only:
 
 ffmpeg -i /storage/emulated/0/ffvid/frameCount.mp4 -vf reverse reversed.mp4
@@ -47,19 +47,20 @@ ffmpeg -i /storage/emulated/0/ffvid/frameCount.mp4 -vf reverse reversed.mp4
 # Reverse audio and video:
 
 ffmpeg -i /storage/emulated/0/ffvid/frameCount.mp4 -vf reverse -af areverse reversed.mp4
-====================================================
 
+#====================================================
 # Create video as a static picture + mp3
 
 ffmpeg -loop 1 -i image.png -i music.mp3 -vf "scale='min(1280,iw)':-2,format=yuv420p" \
 -c:v libx264 -preset medium -profile:v main -c:a aac -shortest -movflags +faststart output.mp4
 
-====================================================
+#====================================================
 # Downloading coub for full length of audio
 # https://www.npmjs.com/package/coub-dl
 
 coub-dl -i https://coub.com/view/135nqc -o out.mp4 --loop 999
-====================================================
+
+#====================================================
 # To list the available formats type and download the chosen one:
 
 youtube-dl -F URL
@@ -70,8 +71,8 @@ youtube-dl --list-subs URL
 
 # To download subs
 youtube-dl --write-sub --sub-lang=en --convert-subs=srt --skip-download URL 
-====================================================
 
+#====================================================
 # Download youtube playlist with en subs
 
 youtube-dl --write-sub --sub-lang=en --convert-subs=ass --skip-download --output "%(title)s.%(ext)s" -i PLpCqYR5zVhaq7k4KJuRXMcjD7PhFHenDL
@@ -84,4 +85,4 @@ youtube-dl -f 18 --output "%(title)s.%(ext)s" -i PLpCqYR5zVhar6AngZd1RIG2_5sTMZs
 
 for i in *.mp4; do ffmpeg -y -i "$i" -vf "ass=${i%%.*}.en.ass" "${i%.*}.en.sub.mp4"; done 
 
-====================================================
+#====================================================
